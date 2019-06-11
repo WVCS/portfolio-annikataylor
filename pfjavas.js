51acb78fc26934b3aab91db2d0e894bf6300ebd7
annikataylor <49955518+annikataylor@users.noreply.github.com>
2019-06-10 20:12:12 -0700

    Update pfjavas.js

--- pfjavas.js
+++ pfjavas.js
@@ -1,24 +1,21 @@
 
-// Modal Image Gallery
 function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
 }
-// Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
 function myFunction() {
-    var navbar = document.getElementById("myNavbar");
+    var topnav = document.getElementById("Nav_Bar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "at-bar" + " at-card" + " at-animate-top" + " at-white";
+        topnav.className = "at-bar" + " at-card" + " at-animate-top" + " at-white";
     } else {
-        navbar.className = navbar.className.replace(" at-card at-animate-top at-white", "");
+        topnav.className = topnav.className.replace(" at-card at-animate-top at-white", "");
     }
 }
-// Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
-    var x = document.getElementById("navDemo");
+    var x = document.getElementById("topnavDemo");
     if (x.className.indexOf("at-show") == -1) {
         x.className += " at-show";
     } else {
